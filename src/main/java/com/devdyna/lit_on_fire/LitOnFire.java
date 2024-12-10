0d632c10bac15af3e523097c766f9454d75288f2
DevDyna <andrearisorti2003@gmail.com>
2024-12-10 14:48:19 +0100

    Update LitOnFire.java

--- src/main/java/com/devdyna/lit_on_fire/LitOnFire.java
+++ src/main/java/com/devdyna/lit_on_fire/LitOnFire.java
@@ -1,11 +1,22 @@
 package com.devdyna.lit_on_fire;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.core.registries.BuiltInRegistries;
+// import net.minecraft.network.chat.ChatType;
+import net.minecraft.network.chat.Component;
+// import net.minecraft.network.chat.OutgoingChatMessage;
+// import net.minecraft.network.chat.PlayerChatMessage;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.neoforged.bus.api.IEventBus;
@@ -21,7 +32,10 @@ public class LitOnFire {
 
     public static final String MODID = "lit_on_fire";
 
-    public static final TagKey<Item> TORCHES = TagKey.create(BuiltInRegistries.ITEM.key(),
+    public static final TagKey<Item> ITEM_TORCHES = TagKey.create(BuiltInRegistries.ITEM.key(),
+            ResourceLocation.fromNamespaceAndPath(MODID, "can_lit_campfire"));
+
+    public static final TagKey<Block> BLOCK_TORCHES = TagKey.create(BuiltInRegistries.BLOCK.key(),
             ResourceLocation.fromNamespaceAndPath(MODID, "can_lit_campfire"));
 
     public LitOnFire(IEventBus modEventBus, ModContainer modContainer) {
@@ -31,19 +45,64 @@ public class LitOnFire {
 
     @SubscribeEvent
     public void TorchclickOnCampfire(PlayerInteractEvent.RightClickBlock event) {
-        if (event.getLevel().getBlockState(event.getPos()).is(BlockTags.CAMPFIRES)
-                &&
-                getState(event).getValue(BlockStateProperties.LIT)
-                && event.getItemStack().is(TORCHES)) {
+
+        Level level = event.getLevel();
+        BlockPos pos = event.getPos();
+
+        if (event.getHand().equals(InteractionHand.MAIN_HAND)
+                && level.getBlockState(pos).is(BlockTags.CAMPFIRES)
+                && event.getItemStack().is(ITEM_TORCHES)
+                && !getState(event).getValue(BlockStateProperties.LIT).booleanValue()) {
+
             event.getEntity().swing(InteractionHand.MAIN_HAND);
-            event.getLevel().setBlockAndUpdate(event.getPos(),
-                    getState(event).setValue(BlockStateProperties.LIT, true));
+
+            if (level.getBlockState(pos.above()).is(BlockTags.AIR)
+                    && level.getBlockState(pos.above().north()).is(BlockTags.AIR)
+                    && level.getBlockState(pos.above().south()).is(BlockTags.AIR)
+                    && level.getBlockState(pos.above().east()).is(BlockTags.AIR)
+                    && level.getBlockState(pos.above().west()).is(BlockTags.AIR)) {
+
+                level.setBlockAndUpdate(pos,
+                        getState(event).setValue(BlockStateProperties.LIT, true));
+                level.addParticle(ParticleTypes.LAVA, true, pos.getX() + 0.5,
+                        pos.getY() + 1.75,
+                        pos.getZ() + 0.5, 0, 0, 0);
+
+                level.playLocalSound(pos.getX(), pos.getY(),
+                        pos.getZ(), SoundEvents.LODESTONE_COMPASS_LOCK, SoundSource.AMBIENT, 100,
+                        0.75f, true);
+
+                messageActionBar("valid");
+            } else {
+
+                level.playLocalSound(pos.getX(), pos.getY(),
+                        pos.getZ(), SoundEvents.VAULT_INSERT_ITEM_FAIL, SoundSource.AMBIENT, 100,
+                        0.75f, true);
+
+                messageActionBar("invalid");
+            }
 
         }
+
     }
 
     public BlockState getState(RightClickBlock event) {
         return event.getLevel().getBlockState(event.getPos());
     }
+
+    @SuppressWarnings({ "resource", "null" })
+    public void messageActionBar(String name) {
+        Minecraft.getInstance().player.displayClientMessage(Component.translatable("actionbar.lit_on_fire." + name),
+                true);
+    }
+
+    // //DEBUG ONLY
+    // public void message(RightClickBlock event, String text) {
+    // event.getEntity().createCommandSourceStack().sendChatMessage(
+    // new OutgoingChatMessage.Player(
+    // PlayerChatMessage.unsigned(event.getEntity().getUUID(), text)),
+    // false, ChatType.bind(ChatType.CHAT, event.getEntity()));
+
+    // }
 
 }
