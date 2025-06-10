9799526b5b52454b277295cdb388a78c594bab8e
DevDyna <andrearisorti2003@gmail.com>
2025-06-10 22:13:10 +0200

    fixed mod not loading

--- src/main/java/com/devdyna/lit_on_fire/Main.java
+++ src/main/java/com/devdyna/lit_on_fire/Main.java
@@ -1,9 +1,9 @@
 package com.devdyna.lit_on_fire;
 
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.ModContainer;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 
 @Mod(Main.MODID)
 public class Main {
@@ -12,8 +12,8 @@ public class Main {
 
     public static String langString = "actionbar." + MODID + ".";
 
-    public Main(IEventBus modEventBus, ModContainer modContainer) {
-        //TODO ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.configBuilder);
+    public Main() {
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.CONFIG_SPEC);
         MinecraftForge.EVENT_BUS.register(new ClickEvent());
     }
 
